9c7a0f07f79279d1c6b582102a52abbd4e18690e
Eduardo Acosta <eacosta@ipcom.com.mx>
2025-12-22 12:28:35 -0600

    Comentarios añadidos

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
   const [products, setProducts] = useState<Product[]>([]); //Se utiliza para guardar los productos
   const [cart, setCart] = useState<Product[]>([]);
 
-  // 1. useEffect pide los productos al Backend de Java
+  // useEffect pide los productos al Backend
   useEffect(() => {
     fetch('http://localhost:8080/api/products')
       .then(res => res.json())
@@ -22,11 +22,12 @@ function App() {
       .catch(err => console.error("Error conectando al backend:", err));
   }, []);
 
-  // 2. Lógica para agregar al carrito (en memoria del cliente)
+  //Lógica para agregar al carrito (en memoria del cliente)
+  //async / await = el programa espera la respuesta del servidor para continuar
 const addToCart = async (product: Product) => {
   console.log("Agregando producto con ID:", product.id, "Cantidad:", product.quantity);
 
-  // Enviamos el producto tal cual, con su cantidad actual
+  // Se envía el producto con su cantidad actual
   const itemParaEnviar = {
     ...product
   };
@@ -39,9 +40,9 @@ const addToCart = async (product: Product) => {
     });
 
     if (!response.ok) {
-      // Si el backend responde con error 400 (Bad Request), obtenemos el mensaje
+      // Si el backend responde con error 400 (Bad Request), se obtiene el mensaje
       const errorData = await response.json();
-      alert("Error: " + errorData.message); // Esto mostrará: "La cantidad debe ser mayor o igual a 1"
+      alert("Error: " + errorData.message); //
     } else {
       refreshCart();
     }
@@ -50,15 +51,17 @@ const addToCart = async (product: Product) => {
   }
 };
 
+
+//vuelve a pedir la lista de todos los productos en el carrito
  const refreshCart = () => {
    fetch('http://localhost:8080/api/cart/user1')
      .then(res => res.json())
      .then(data => setCart(data));
  };
 
-  // 3. Quitar del carrito (Llamada al Backend)
+  //Obtiene el id del producto a borrar y lo envia al cartservice, si el back retorna como exitoso el borrado se actualiza la pantalla
   const removeFromCart = async (productId: number) => {
-    console.log("Intentando borrar producto con ID:", productId); // Revisa esto en la consola (F12)
+    console.log("Intentando borrar producto con ID:", productId);
 
     try {
       const response = await fetch(`${import.meta.env.VITE_API_URL}/cart/user1/items/${productId}`, {
@@ -66,7 +69,7 @@ const addToCart = async (product: Product) => {
       });
 
       if (response.ok) {
-        refreshCart(); // Esto vuelve a pedir el carrito al servidor
+        refreshCart();
       }
     } catch (error) {
       console.error("Error al borrar:", error);
@@ -75,7 +78,25 @@ const addToCart = async (product: Product) => {
 
   return (
     <div className="container">
-      <h1>🛒 Tienda Practica 2 (Java 25 + React)</h1>
+      <h1>Practica 2</h1>
+
+
+{/* SECCIÓN DEL CARRITO */}
+      <div className="cart-section">
+        <h2>Tu Carrito ({cart.length} ítems)</h2>
+        {cart.length === 0 ? <p>El carrito está vacío.</p> : (
+          <ul>
+            {cart.map((item, index) => (
+              <li key={index} style={{ marginBottom: '10px' }}>
+                {item.name} - ${item.price}
+                <button className="btn btn-delete" style={{ marginLeft: '10px' }} onClick={() => removeFromCart(item.id)}>Quitar</button>
+              </li>
+            ))}
+          </ul>
+        )}
+        <h3>Total: ${cart.reduce((acc, item) => acc + item.price, 0)}</h3>
+      </div>
+
 
       {/* SECCIÓN DE PRODUCTOS */}
       <div className="grid">
@@ -94,21 +115,7 @@ const addToCart = async (product: Product) => {
         ))}
       </div>
 
-      {/* SECCIÓN DEL CARRITO */}
-      <div className="cart-section">
-        <h2>Tu Carrito ({cart.length} ítems)</h2>
-        {cart.length === 0 ? <p>El carrito está vacío.</p> : (
-          <ul>
-            {cart.map((item, index) => (
-              <li key={index} style={{ marginBottom: '10px' }}>
-                {item.name} - ${item.price}
-                <button className="btn btn-delete" style={{ marginLeft: '10px' }} onClick={() => removeFromCart(item.id)}>Quitar</button>
-              </li>
-            ))}
-          </ul>
-        )}
-        <h3>Total: ${cart.reduce((acc, item) => acc + item.price, 0)}</h3>
-      </div>
+
     </div>
   );
 }
