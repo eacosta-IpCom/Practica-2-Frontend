61208799d23ce60f25f4acafdabccbc063864d3b
acmoed <acmoed@gmail.com>
2025-12-22 15:25:18 -0600

    Cambios a la rama pruebasGitFrontend desde acmoed prueba 2

--- src/App.tsx
+++ src/App.tsx
@@ -78,7 +78,7 @@ const addToCart = async (product: Product) => {
 
   return (
     <div className="container">
-      <h1>Practica 2 cambios a la rama pruebasGitFrontend desde acmoed </h1>
+      <h1>Practica 2 cambios a la rama pruebasGitFrontend desde acmoed  Prurba2</h1>
 
 
 {/* SECCIÓN DEL CARRITO */}
